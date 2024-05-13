629e66d1f700379f3e16c68a2203c7468138bbb8
Sandip Gahlot <sgahlot@redhat.com>
2024-05-13 16:31:35 -0400

    testing customization of entity-feedback - 4

---
+++ plugins/entity-feedback/overlay/src/custom.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {InfoCard} from '@backstage/core-components';
+import { LikeDislikeButtons } from '@backstage/plugin-entity-feedback';
+
+// export * from '@backstage/plugin-entity-feedback';
+
+export const CustomLikeDislikeCard = () => (
+    <InfoCard title="Rate this entity">
+       <LikeDislikeButtons/>
+    </InfoCard>
+  );
