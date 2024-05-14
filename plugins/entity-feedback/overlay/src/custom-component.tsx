e6b2b1377b886710ee4ca081a31b3a4d49cf4463
Sandip Gahlot <sgahlot@redhat.com>
2024-05-14 10:15:09 -0400

    working version on local

--- plugins/entity-feedback/overlay/src/custom-component.tsx
+++ plugins/entity-feedback/overlay/src/custom-component.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import {InfoCard} from '@backstage/core-components';
 import { LikeDislikeButtons } from '@backstage/plugin-entity-feedback';
 
-// export * from '@backstage/plugin-entity-feedback';
-
 export const CustomLikeDislikeCard = () => (
     <InfoCard title="Rate this entity">
        <LikeDislikeButtons/>
--- plugins/entity-feedback/overlay/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from '@backstage/plugin-entity-feedback';
-
-// export {CustomLikeDislikeCard} from './custom';
